23023481926cbaae6de127fa2f575d4b376b6493
Neil Edwards <neil@plasticwater.com>
2015-03-04 23:11:38 -0500

    updated logic

--- bin/server.js
+++ bin/server.js
@@ -5,6 +5,7 @@ module.exports = function(options) {
   var debug = require('debug')('mayor');
   var app = require('../app');
   var models = require("../models");
+  var renderApplication = require("../config/simple.js");
 
   app.set('port', process.env.PORT || 3000);
   app.set('ip', process.env.IP || "0.0.0.0");
@@ -16,6 +17,15 @@ module.exports = function(options) {
   var STYLE_URL = options.separateStylesheet && (publicPath + "main.css?" + stats.hash);
   var SCRIPT_URL = publicPath + [].concat(stats.assetsByChunkName.main)[0];
   var COMMONS_URL = publicPath + [].concat(stats.assetsByChunkName.commons)[0];
+
+
+  // application
+	app.get("/*", function(req, res) {
+		renderApplication(req.path, SCRIPT_URL, STYLE_URL, COMMONS_URL, function(err, html) {
+			res.contentType = "text/html; charset=utf8";
+			res.end(html);
+		});
+	});
 
   models.sequelize.sync().then(function () {
     var server = app.listen(app.get('port'), app.get('ip'), function() {
