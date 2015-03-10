60bc0a43a31db293563473871e15152b3bf9c4e2
Neil Edwards <neil@plasticwater.com>
2015-03-10 16:07:07 -0400

    use heroku postgres

--- models/index.js
+++ models/index.js
@@ -3,9 +3,16 @@
 var fs        = require("fs");
 var path      = require("path");
 var Sequelize = require("sequelize");
-var env       = process.env.NODE_ENV || "development";
-var config    = require(__dirname + '/../config/config.json')[env];
-var sequelize = new Sequelize(config.database, config.username, config.password, config);
+//var env       = process.env.NODE_ENV || "development";
+//var config    = require(__dirname + '/../config/config.json')[env];
+var sequelize = new Sequelize(process.env.DATABASE_URL, {
+      dialect:  'postgres',
+      protocol: 'postgres',
+      port:     match[4],
+      host:     match[3],
+      logging:  true //false
+    })
+
 var db        = {};
 
 fs
