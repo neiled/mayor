e369cb7053ce677498969f0067107ebb4e6faee3
Neil Edwards <neil@plasticwater.com>
2015-03-16 14:38:30 +0000

    update index to remove old

--- app/Application/index.jsx
+++ app/Application/index.jsx
@@ -8,7 +8,7 @@ var RouteHandler = Router.RouteHandler;
 
 require('../css/mayor.css');
 require('../css/nifty.min.css');
-require('../js/nifty.min.js');
+// require('../js/nifty.min.js');
 
 
 
@@ -17,49 +17,49 @@ var MainNav = require('./MainNav');
 
 
 var App = React.createClass({
-  getInitialState: function() {
-    return {user: null, total:0, coins:0};
-  },
-  componentDidMount: function() {
-    $.get("/user/current", function(result) {
-      if(this.isMounted()) {
-        this.setState({user: result});
-        var socket = io.connect();
-        socket.on('inventory:update:'+result.user.id, function (data) {
-          this.setState({total:data.total, coins: data.coins})
-        }.bind(this));
-      }
-    }.bind(this));
-    $.get("/user/inventory", function(result) {
-      if(this.isMounted()) {
-        this.setState({total: result.total});
-        this.setState({coins: result.coins});
-      }
-    }.bind(this));
+  // getInitialState: function() {
+  //   return {user: null, total:0, coins:0};
+  // },
+  // componentDidMount: function() {
+  //   $.get("/user/current", function(result) {
+  //     if(this.isMounted()) {
+  //       this.setState({user: result});
+  //       var socket = io.connect();
+  //       socket.on('inventory:update:'+result.user.id, function (data) {
+  //         this.setState({total:data.total, coins: data.coins})
+  //       }.bind(this));
+  //     }
+  //   }.bind(this));
+  //   $.get("/user/inventory", function(result) {
+  //     if(this.isMounted()) {
+  //       this.setState({total: result.total});
+  //       this.setState({coins: result.coins});
+  //     }
+  //   }.bind(this));
 
-  },
-  renderSidebar: function() {
-    if(this.state.user && Object.keys(this.state.user).length !== 0)
-    {
-      return (
-        <div className="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar">
-          <div className="list-group">
-            <Link className="list-group-item" to="hunting">Hunting</Link>
-            <Link className="list-group-item" to="fishing">Fishing</Link>
-            <Link className="list-group-item" to="diving">Diving</Link>
-            <Link className="list-group-item" to="gardening">Gardening</Link>
-            <Link className="list-group-item" to="museum">Museum</Link>
-            <Link className="list-group-item" to="store">Store</Link>
-            <Link className="list-group-item" to="construction">Construction</Link>
-          </div>
-          <div className="list-group">
-            <Link className="list-group-item" to="inventory">Pockets <span className="badge">{this.state.total}</span></Link>
-            <Link className="list-group-item" to="coins">Coins <span className="badge">{this.state.coins}</span></Link>
-          </div>
-        </div>
-      );
-    }
-  },
+  // },
+  // renderSidebar: function() {
+  //   if(this.state.user && Object.keys(this.state.user).length !== 0)
+  //   {
+  //     return (
+  //       <div className="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar">
+  //         <div className="list-group">
+  //           <Link className="list-group-item" to="hunting">Hunting</Link>
+  //           <Link className="list-group-item" to="fishing">Fishing</Link>
+  //           <Link className="list-group-item" to="diving">Diving</Link>
+  //           <Link className="list-group-item" to="gardening">Gardening</Link>
+  //           <Link className="list-group-item" to="museum">Museum</Link>
+  //           <Link className="list-group-item" to="store">Store</Link>
+  //           <Link className="list-group-item" to="construction">Construction</Link>
+  //         </div>
+  //         <div className="list-group">
+  //           <Link className="list-group-item" to="inventory">Pockets <span className="badge">{this.state.total}</span></Link>
+  //           <Link className="list-group-item" to="coins">Coins <span className="badge">{this.state.coins}</span></Link>
+  //         </div>
+  //       </div>
+  //     );
+  //   }
+  // },
   render: function () {
     // var authLinks = this.renderAuthLinks();
     // var sidebar = this.renderSidebar();
