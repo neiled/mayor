3168cb4fc342c4857fa6d3372ad37ebebbf474e8
Neil Edwards <neil@plasticwater.com>
2015-03-16 20:13:57 +0000

    fix buttons

--- app/Welcome.jsx
+++ app/Welcome.jsx
@@ -6,23 +6,23 @@ var Welcome = React.createClass({
     return (
       <div>
         <div id="page-title">
-          <h1 className="page-header text-overflow">Welcome</h1>
+          <h1 className="page-header text-overflow">Welcome to your new town</h1>
         </div>
-        <h2>Welcome to your new town.</h2>
-        <div className="row">
-          <div className="col-lg-7">
-            <div className="panel">
-              <div className="panel-heading">
-								<h3 className="panel-title">Log In</h3>
-							</div>
-							<div className="panel-body">
-                <a href="/auth/twitter"><button className="btn btn-primary btn-labeled icon-lg fa fa-twitter">Twitter</button></a>
-                <a href="/auth/github"><button className="btn btn-primary btn-labeled icon-lg fa fa-github">GitHub</button></a>
-              </div>
-            </div>          
+        <div id="page-content">
+          <div className="row">
+            <div className="col-lg-7">
+              <div className="panel">
+                <div className="panel-heading">
+    							<h3 className="panel-title">Log In</h3>
+    						</div>
+    						<div className="panel-body">
+                  <a href="/auth/twitter" className="btn btn-primary btn-labeled icon-lg fa fa-twitter">Twitter</a>
+                  <a href="/auth/github" className="btn btn-primary btn-labeled icon-lg fa fa-github">GitHub</a>
+                </div>
+              </div>          
+            </div>
           </div>
-        </div>
-
+        </div>        
       </div>
     );
   }
