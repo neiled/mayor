b6a8bda6bed57a76247c218674b8666e6fd78241
Neil Edwards <neil@plasticwater.com>
2015-03-16 19:29:11 +0000

    fix class name

--- app/Welcome.jsx
+++ app/Welcome.jsx
@@ -9,14 +9,14 @@ var Welcome = React.createClass({
           <h1 className="page-header text-overflow">Welcome</h1>
         </div>
         <h2>Welcome to your new town.</h2>
-        <div class="row">
-          <div class="col-lg-7">
-            <div class="panel">
-              <div class="panel-heading">
-								<h3 class="panel-title">Log In</h3>
+        <div className="row">
+          <div className="col-lg-7">
+            <div className="panel">
+              <div className="panel-heading">
+								<h3 className="panel-title">Log In</h3>
 							</div>            
-              <a href="/auth/twitter"><button class="btn btn-primary btn-labeled icon-lg fa fa-twitter">Twitter</button></a>
-              <a href="/auth/github"><button class="btn btn-primary btn-labeled icon-lg fa fa-github">GitHub</button></a>
+              <a href="/auth/twitter"><button className="btn btn-primary btn-labeled icon-lg fa fa-twitter">Twitter</button></a>
+              <a href="/auth/github"><button className="btn btn-primary btn-labeled icon-lg fa fa-github">GitHub</button></a>
             </div>          
           </div>
         </div>
