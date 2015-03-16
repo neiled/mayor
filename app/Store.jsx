35271bdcdfde0bb71857bbddd1f813341b57e55d
Neil Edwards <neil@plasticwater.com>
2015-03-15 21:51:25 -0400

    added boostrap react and tabbed area for store

--- app/Store.jsx
+++ app/Store.jsx
@@ -1,14 +1,23 @@
 var React = require("react");
 var request = require('superagent');
+var TabbedArea = require('react-bootstrap').TabbedArea;
+var TabPane = require('react-bootstrap').TabPane;
 
 var Store = React.createClass({
   render: function() {
+  var tabbedAreaInstance = (
+      <TabbedArea defaultActiveKey={1}>
+        <TabPane eventKey={1} tab="Buy">Buy</TabPane>
+        <TabPane eventKey={2} tab="Sell">Sell</TabPane>
+      </TabbedArea>
+    );
     return (
       <div>
         <div className="jumbotron">
           <h2>Store</h2>
           <p>Time for some retail therapy...</p>
         </div>
+        {tabbedAreaInstance}
       </div>
     );
   }
